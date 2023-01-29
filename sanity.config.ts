b9f9230970ed25f4981fa4d8afd76fc055122fc3
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-29 15:57:17 +0100

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,17 +1,21 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
+
 import {schemaTypes} from './schemas'
+// import {deskStructure} from './deskStructure'
 
 export default defineConfig({
   name: 'default',
   title: 'robinwittkamp.com',
-
   projectId: 'rx62v1yc',
   dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
+  plugins: [
+    deskTool({
+      // structure: deskStructure,
+    }),
+    visionTool(),
+  ],
   schema: {
     types: schemaTypes,
   },
