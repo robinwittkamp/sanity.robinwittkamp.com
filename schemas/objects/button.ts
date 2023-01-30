b2e5201952bacae49ad97abcc39b4067195fbb11
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-30 17:55:51 +0100

    Update button.ts

--- schemas/objects/button.ts
+++ schemas/objects/button.ts
@@ -1,17 +1,19 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: "button",
   title: "Button",
   type: "object",
   fields: [
-    {
+    defineField({
       name: 'text',
       title: 'text',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: "link",
       title: "Link",
       type: "link",
-    }
+    }),
   ]
-};
+})
