a4e22fd5c3e29d4abc9685e327a007f57e4e46a7
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-30 17:55:53 +0100

    Update heroSection.ts

--- schemas/objects/heroSection.ts
+++ schemas/objects/heroSection.ts
@@ -1,29 +1,31 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: "hero",
   title: "Hero",
   type: "object",
   fields: [
-    {
+    defineField({
       name: 'tagline',
       title: 'Tagline',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'heading',
       title: 'Heading',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'subheading',
       title: 'Subheading',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: "button",
       title: "Button",
       type: "button",
-    },
-    {
+    }),
+    defineField({
       name: 'image',
       title: 'Image',
       type: 'image',
@@ -37,6 +39,6 @@ export default {
           type: 'string',
         }
       ]
-    }
+    }),
   ]
-}
+})
