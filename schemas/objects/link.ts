ec0b1526cc35ef9c1df9d401c49767c44615eec1
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-01-30 17:55:56 +0100

    Update link.ts

--- schemas/objects/link.ts
+++ schemas/objects/link.ts
@@ -1,19 +1,20 @@
-export default {
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
   name: "link",
   title: "Link",
   type: "object",
   fields: [
-    {
+    defineField({
       name: "external",
       title: "Link to external URL",
       type: "boolean"
-    },
-    {
+    }),
+    defineField({
       name: "url",
       title: "URL",
-      description:
-        "For external links use the full url e.g. ‘https://google.com/’. For internal links use the page path e.g. ‘companies’",
-      type: "string"
-    }
+      type: "url",
+      validation: Rule => Rule.uri({ allowRelative: true }),
+    }),
   ]
-};
+})
