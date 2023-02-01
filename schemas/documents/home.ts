f20f305b8a5d4c9ca705812d3478e30a529b432e
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-01 15:56:17 +0100

    Create home.ts

---
+++ schemas/documents/home.ts
@@ -0,0 +1,25 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'home',
+  title: 'Home',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'hero',
+      title: 'Name',
+      type: 'string',
+    }),
+    // defineField({
+    //   name: "sections",
+    //   title: "Sections",
+    //   type: "array",
+    //   of: [{ type: "heroSection" }]
+    // }),
+  ],
+  // preview: {
+  //   select: {
+  //     title: 'home',
+  //   },
+  // },
+})
