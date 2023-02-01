5aeca61328468691617444a80452adeb015347be
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-01 15:56:35 +0100

    Load all schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -3,16 +3,22 @@ import blockContent from './arrays/blockContent'
 
 // Document types
 import author from './documents/author'
+import home from './documents/home'
 import navigation from './documents/navigation'
 import post from './documents/post'
 
 // Object types
-import navigationItem from './objects/navigationItem'
+import button from './objects/button'
+import heroSection from './objects/sections/heroSection'
 import link from './objects/link'
+import navigationItem from './objects/navigationItem'
 
 export const schemaTypes = [
   author,
+  button,
   blockContent,
+  heroSection,
+  home,
   link,
   navigation,
   navigationItem,
