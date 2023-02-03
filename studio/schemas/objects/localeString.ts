409e0a27ffd44c1d3a9d6ff131decf5f16914594
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-03 19:51:38 +0100

    Create localeString.ts

---
+++ studio/schemas/objects/localeString.ts
@@ -0,0 +1,38 @@
+import {defineField, defineType} from 'sanity'
+
+import {supportedLanguages} from '../../supportedLanguages'
+
+export default defineType({
+  name: 'localeString',
+  title: 'Localized String',
+  type: 'object',
+  // Fieldsets can be used to group object fields.
+  // Here we omit a fieldset for the "default language",
+  // making it stand out as the main field.
+  fieldsets: [
+    {
+      title: 'Translations',
+      name: 'translations',
+      options: { collapsible: true }
+    }
+  ],
+  // Dynamically define one field per language
+  fields: supportedLanguages.map(lang => ({
+    title: lang.title,
+    name: lang.id,
+    type: 'string',
+    fieldset: lang.isDefault ? null : 'translations'
+  }))
+  // fields: [
+  //   defineField({
+  //     name: 'en',
+  //     title: 'English',
+  //     type: 'string',
+  //   }),
+  //   defineField({
+  //     name: "de",
+  //     title: "German",
+  //     type: "string",
+  //   }),
+  // ],
+})
