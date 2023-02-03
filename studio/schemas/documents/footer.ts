5634b7f3408ff0be6985de0fe6e7c49122728f20
Robin Wittkamp <robinwittkamp94@gmail.com>
2023-02-03 15:28:46 +0100

    Add footer schema

---
+++ studio/schemas/documents/footer.ts
@@ -0,0 +1,44 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'footer',
+  title: 'Footer',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'logo',
+      title: 'Logo',
+      type: 'image',
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+      name: "mainNavigation",
+      title: "Main Navigation",
+      type: "navigation",
+    }),
+    defineField({
+      name: "legalNavigation",
+      title: "Legal Navigation",
+      type: "navigation",
+    }),
+    defineField({
+      name: "usedTechnologies",
+      title: "Note about used Technologies",
+      type: "string",
+    }),
+    defineField({
+      name: "RepoHint",
+      title: "Note about the repository",
+      type: "string",
+    }),
+  ],
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //   },
+  // },
+})
